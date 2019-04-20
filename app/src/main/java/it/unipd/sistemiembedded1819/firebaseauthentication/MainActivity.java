425c7fe8a904a3b8c7eff294612afb4748d58ca7
AlbVal97 <alberto.valente.3@studenti.unipd.it>
2019-04-20 21:07:50 +0200

    Added Firebase authentication class

---
+++ app/src/main/java/it/unipd/sistemiembedded1819/firebaseauthentication/MainActivity.java
@@ -0,0 +1,13 @@
+package it.unipd.sistemiembedded1819.firebaseauthentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
